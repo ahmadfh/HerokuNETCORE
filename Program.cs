775082fc12d952926cca8214b6fb7623015daa66
Ahmad Hamed <ahmad.fhamed@gmail.com>
2018-05-04 16:19:35 +0300

    tests 2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace test
+namespace adsasd
 {
     public class Program
     {
